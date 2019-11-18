49c8e4a2d9b287099ca678db1ed57380cb44186e
Adam Blunt <adamxp12@adamxp12.com>
2019-11-18 15:56:33 +0000

    Change from TCPClient to Socket as new lines was not working correctly

--- EscPos/TcpPrinter.cs
+++ EscPos/TcpPrinter.cs
@@ -1,21 +1,22 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Windows.Forms;
 
-namespace EscPos
+namespace PushPOS.printer
 {
-    public class TcpPrinter : PrinterInterface
+    public class tcpPrinter : PrinterInterface
     {
-        TcpClient c = new TcpClient();
-        NetworkStream stream;
+
+        private IPEndPoint eP;
+        private Socket s;
 
         private String printerHost;
         private Int32 printerPort;
 
-        public TcpPrinter(String pHost, Int32 pPort)
+        public tcpPrinter(String pHost, Int32 pPort)
         {
             printerPort = pPort;
             printerHost = pHost;
@@ -23,45 +24,41 @@ namespace EscPos
 
         public void Close()
         {
-            stream.Close();
-            c.Close();
+            s.Close();
         }
 
         public void Init()
         {
             try
             {
-                c.SendTimeout = 3000;
-                c.ReceiveTimeout = 3000;
-                c.Connect(printerHost, printerPort);
-                stream = c.GetStream();
-            }
-            catch (Exception ex)
+                eP = new IPEndPoint(IPAddress.Parse(printerHost), printerPort);
+                s = new Socket(eP.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                s.Connect(eP);
+
+            } catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "Error enabling printer");
                 Console.WriteLine(ex);
             }
-
+            
         }
 
         public bool IsOpen()
         {
-            return c.Connected;
+            return s.Connected;
         }
 
         public void Write(string line)
         {
-            stream.Write(Encoding.Unicode.GetBytes(line), 0, line.Length);
-            stream.Flush();
+            s.Send(Encoding.UTF8.GetBytes(line));
         }
 
         public void Write(List<string> lines)
         {
             foreach (string line in lines)
             {
-                stream.Write(Encoding.Unicode.GetBytes(lines.ToString()), 0, lines.ToString().Length);
+                s.Send(Encoding.UTF8.GetBytes(line));
             }
-            stream.Flush();
         }
     }
 }
